7b78881c9ad2679f32c4bf0af42517844e97e33a
kartik <raghu1ckartik.rk@gmail.com>
2024-12-29 17:21:57 +0530

    Initial commit1

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,41 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    vector<int> v;
-    vector<int> v1;
-    for(int j=0;j<5;j++){
-        v1.emplace_back(2*j);
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        v.push_back(i);
-    }
-    
 
-
-    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-    {
-        cout << *(it) << " ";
+string twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return "YES";
+        }
+        mpp[num] = i;
     }
-
-    cout<<endl;
-
-    for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
-    {
-        cout << *(it) << " ";
-    }
-
-    v.insert(v.begin() + 5, 69);
-    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-    {
-        cout << *(it) << " ";
-    }
-
-    v.insert(v.begin(),v1.begin(),v1.end());
-    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-    {
-        cout << *(it) << " ";
-    }
-    v.erase(v.begin()+5,v.begin()+9);
+    return "NO";
 }
